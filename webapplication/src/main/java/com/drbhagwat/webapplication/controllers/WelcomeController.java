979624bdb149c7ef0478f0bc7029b1ac543ddc49
Bhoomika Bhagwat <Bhoomika Bhagwat@DESKTOP-Q6TS8A5>
2018-12-02 19:58:31 +0530

    making sure the latest is present

--- webapplication/src/main/java/com/drbhagwat/webapplication/controllers/WelcomeController.java
+++ webapplication/src/main/java/com/drbhagwat/webapplication/controllers/WelcomeController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
-	@RequestMapping("/welcome.html")
+	@RequestMapping("/")
 	public String Welcome() {
-		return ("welcome.jsp");
+		return ("index.jsp");
 	}
 }
