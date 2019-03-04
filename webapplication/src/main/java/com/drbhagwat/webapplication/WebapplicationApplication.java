207d9d6d50aa6fc92ae127af3560902014983006
Dinesh <drbhagwat@yahoo.com>
2019-03-04 14:20:32 +0530

    Final commit

--- webapplication/src/main/java/com/drbhagwat/webapplication/WebapplicationApplication.java
+++ webapplication/src/main/java/com/drbhagwat/webapplication/WebapplicationApplication.java
@@ -2,11 +2,14 @@ package com.drbhagwat.webapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:spring-config.xml")
 public class WebapplicationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebapplicationApplication.class, args);
-	}
+  public static void main(String[] args) {
+	SpringApplication.run(WebapplicationApplication.class, args);
+  }
 }
